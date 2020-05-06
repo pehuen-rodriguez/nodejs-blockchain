14e20050ceda8d3c2e63495b541f453c42e4f8a5
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-06 14:24:45 -0300

    Some match login on the orderbook

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const { Orderbook } = require("./orderbook");
 function App() {
   this.orderbook = new Orderbook();
 
-  this.orderbook.on("match", () => {});
+  this.orderbook.on("match", () => {
+    console.log("there was a match");
+  });
 
   this.init = () => {
     // Connect here to the grape?
@@ -24,3 +26,35 @@ function App() {
 }
 
 module.exports = { App };
+
+// it'll be a server and a client. Would I use PubSub?
+// const { PeerRPCServer } = require("grenache-nodejs-ws");
+// const Link = require("grenache-nodejs-link");
+
+// function fibonacci(n) {
+//   if (n <= 1) {
+//     return 1;
+//   }
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+// const link = new Link({
+//   grape: "http://127.0.0.1:30001",
+// });
+
+// link.start();
+
+// const peer = new PeerRPCServer(link, {});
+// peer.init();
+
+// const service = peer.transport("server");
+// service.listen(1337);
+
+// setInterval(() => {
+//   link.announce("fibonacci_worker", service.port, {});
+// }, 1000);
+
+// service.on("request", (rid, key, payload, handler) => {
+//   const result = fibonacci(payload.number);
+//   handler.reply(null, result);
+// });
--- orderbook.js
+++ orderbook.js
@@ -14,15 +14,50 @@ function Orderbook() {
   };
 
   this.on("putOrder", (order) => {
-    this.availableorders++;
+    this.availableOrders++;
     // will fetch buy and sell for that price
-    const bucket = get_bucket(order);
-    bucket[order.type].push(order);
+    const bucket = getBucket(order);
+    const oppositeOrders = bucket[opposite(order.type)];
+
+    if (oppositeOrders && oppositeOrders.length > 0) {
+      let i = 0;
+      let opposite = oppositeOrders[i];
+      let matchedAmount = opposite.amount;
+
+      // go through opposite orders
+      while (i + 1 < oppositeOrders.length && opposite.amount <= order.amount) {
+        opposite = oppositeOrders[++i];
+        matchedAmount += opposite.amount;
+      }
+
+      // remove them from the list
+      const oppositeOrdersMatched = oppositeOrders.splice(0, i + 1);
+
+      if (matchedAmount < order.amount) {
+        // there's still amount left
+        this.availableOrders -= i + 1;
+        bucket[order.type].push(order);
+        this.emit(
+          "match",
+          {...order, amount: order.amount - matchedAmount},
+          oppositeOrdersMatched
+        );
+      } else if (matchedAmount === order.amount) {
+        this.availableOrders -= i + 2;
+        this.emit("match", order, oppositeOrdersMatched);
+      } else {
+        // what if
+      }
+    } else {
+      // no options. Just push it
+      bucket[order.type].push(order);
+    }
 
     console.log(bucket);
   });
 
-  const get_bucket = (order) => {
+  const getBucket = (order) => {
+    // get me the list of orders for the corresponding price: a bucket
     if (this.orderbook[order.price] === undefined) {
       this.orderbook[order.price] = {
         buy: [],
@@ -30,6 +65,10 @@ function Orderbook() {
       };
     }
     return this.orderbook[order.price];
+  };
+
+  const opposite = (type) => {
+    type === "sell" ? "buy" : "sell";
   };
 }
 
